c23fe820306cf812186d46a931308944ec2bd411
Nicholas Ruas Viegas <nicholasrv@live.com>
2023-08-01 23:08:22 -0300

    Building up schema/application properties

---
+++ src/main/java/dev/nicholasrv/dgtlbilling/DgtlbillingApplication.java
@@ -0,0 +1,13 @@
+package dev.nicholasrv.dgtlbilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgtlbillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgtlbillingApplication.class, args);
+	}
+
+}
