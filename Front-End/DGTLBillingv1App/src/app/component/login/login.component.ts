84879b67b98a091512d1ace47deeabb87942643c
Nicholas Ruas Viegas <69402500+nicholasrv@users.noreply.github.com>
2023-08-27 21:06:56 -0300

    setting up main application states + login component - pt. 1

--- Front-End/DGTLBillingv1App/src/app/component/login/login.component.ts
+++ Front-End/DGTLBillingv1App/src/app/component/login/login.component.ts
@@ -1,10 +1,66 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import {
+  BehaviorSubject,
+  Observable,
+  catchError,
+  map,
+  of,
+  startWith,
+} from 'rxjs';
+import { DataState } from 'src/app/enum/datastate.enum';
+import { Key } from 'src/app/enum/key.enum';
+import { LoginState } from 'src/app/interface/appstates';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  loginState$ = (Observable<LoginState> = of({ dataState: DataState.LOADED }));
+  private phoneSubject = new BehaviorSubject<string | null>(null);
+  private emailSubject = new BehaviorSubject<string | null>(null);
 
+  constructor(private router: Router, private userService: UserService) {}
+
+  login(loginForm: NgForm): void {
+    this.loginState$ = this.userService
+      .login$(loginForm.value.email, loginForm.value.password)
+      .pipe(
+        map((response) => {
+          if (response.data.user.isUsingMfa) {
+            this.phoneSubject.next(response.data.user.phone);
+            this.emailSubject.next(response.data.user.email);
+            return {
+              dataState: DataState.LOADED,
+              isUsingMfa: true,
+              loginSuccess: false,
+              phone: response.data.user.phone.substring(
+                response.data.user.phone.length - 4
+              ),
+            };
+          } else {
+            localStorage.setItem(Key.TOKEN, response.data.access_token);
+            localStorage.setItem(
+              Key.REFRESH_TOKEN,
+              response.data.refresh_token
+            );
+            this.router.navigate(['/']);
+            return { dataState: DataState.LOADED, isUsingMfa: true };
+          }
+        }),
+        startWith({ dataState: DataState.LOADING, isUsingMfa: false }),
+        catchError((error: string) => {
+          return of({
+            dataState: DataState.ERROR,
+            isUsingMfa: false,
+            loginSuccess: false,
+            error,
+          });
+        })
+      );
+  }
 }
---
+++ Front-End/DGTLBillingv1App/src/app/service/user.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { CustomHttpResponse, Profile } from '../interface/appstates';
+import { Observable, catchError, tap, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private readonly server: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  login$ = (email: string, password: string) =>
+    <Observable<CustomHttpResponse<Profile>>>this.http
+      .post<CustomHttpResponse<Profile>>(`${this.server}/user/login`, {
+        email,
+        password,
+      })
+      .pipe(tap(console.log), catchError(this.handleError));
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    let errorMessage: string;
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `A client error occurred - ${error.error.message}`;
+    } else {
+      if (error.error.reason) {
+        errorMessage = error.error.reason;
+      } else {
+        errorMessage = `An error occurred - Error code ${error.status}`;
+      }
+    }
+    return throwError(() => errorMessage);
+  }
+}
